9916ca3b1a8e3359b5aaf8e5e8ff5bc8e16fbedb
HyFran <hyfrancn@gmail.com>
2024-08-08 14:55:42 +0800

    lab6 finished.

--- lab6/capers/CapersRepository.java
+++ lab6/capers/CapersRepository.java
@@ -1,46 +1,65 @@
 package capers;
 
 import java.io.File;
-import static capers.Utils.*;
+import java.io.IOException;
 
-/** A repository for Capers 
- * @author TODO
+/**
+ * A repository for Capers
+ *
+ * @author Gibson Yee
  * The structure of a Capers Repository is as follows:
- *
+ * <p>
  * .capers/ -- top level folder for all persistent data in your lab12 folder
- *    - dogs/ -- folder containing all of the persistent data for dogs
- *    - story -- file containing the current story
- *
+ * - dogs/ -- folder containing all of the persistent data for dogs
+ * - story -- file containing the current story
+ * <p>
  * TODO: change the above structure if you do something different.
  */
 public class CapersRepository {
-    /** Current Working Directory. */
+    /**
+     * Current Working Directory.
+     */
     static final File CWD = new File(System.getProperty("user.dir"));
 
-    /** Main metadata folder. */
-    static final File CAPERS_FOLDER = null; // TODO Hint: look at the `join`
-                                            //      function in Utils
+    /**
+     * Main metadata folder.
+     */
+    static final File CAPERS_FOLDER = Utils.join(CWD, ".capers");
+    //      function in Utils
+
+    static final File story = Utils.join(CAPERS_FOLDER, "story");
 
     /**
      * Does required filesystem operations to allow for persistence.
      * (creates any necessary folders or files)
      * Remember: recommended structure (you do not have to follow):
-     *
+     * <p>
      * .capers/ -- top level folder for all persistent data in your lab12 folder
-     *    - dogs/ -- folder containing all of the persistent data for dogs
-     *    - story -- file containing the current story
+     * - dogs/ -- folder containing all of the persistent data for dogs
+     * - story -- file containing the current story
      */
     public static void setupPersistence() {
-        // TODO
+        CAPERS_FOLDER.mkdir();
+        Dog.DOG_FOLDER.mkdir();
+        try {
+            if (!story.exists()) {
+                story.createNewFile();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
      * Appends the first non-command argument in args
      * to a file called `story` in the .capers directory.
+     *
      * @param text String of the text to be appended to the story
      */
     public static void writeStory(String text) {
-        // TODO
+        String story = Utils.readContentsAsString(CapersRepository.story) + text + '\n';
+        Utils.writeContents(CapersRepository.story, story);
+        System.out.println(story);
     }
 
     /**
@@ -49,16 +68,23 @@ public class CapersRepository {
      * Also prints out the dog's information using toString().
      */
     public static void makeDog(String name, String breed, int age) {
-        // TODO
+        Dog dog = new Dog(name, breed, age);
+        System.out.println(dog);
+        dog.saveDog();
     }
 
     /**
      * Advances a dog's age persistently and prints out a celebratory message.
      * Also prints out the dog's information using toString().
      * Chooses dog to advance based on the first non-command argument of args.
+     *
      * @param name String name of the Dog whose birthday we're celebrating.
      */
     public static void celebrateBirthday(String name) {
         // TODO
+        File dogFile = Utils.join(Dog.DOG_FOLDER, name);
+        Dog dog = Dog.fromFile(name);
+        dog.haveBirthday();
+        dog.saveDog();
     }
 }
--- lab6/capers/Dog.java
+++ lab6/capers/Dog.java
@@ -1,30 +1,41 @@
 package capers;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.Serializable;
-import static capers.Utils.*;
 
-/** Represents a dog that can be serialized.
+/**
+ * Represents a dog that can be serialized.
+ *
  * @author TODO
-*/
-public class Dog { // TODO
+ */
+public class Dog implements Serializable {
 
-    /** Folder that dogs live in. */
-    static final File DOG_FOLDER = null; // TODO (hint: look at the `join`
-                                         //      function in Utils)
+    /**
+     * Folder that dogs live in.
+     */
+    static final File DOG_FOLDER = Utils.join(CapersRepository.CAPERS_FOLDER, "dogs");
+    //      function in Utils)
 
-    /** Age of dog. */
+    /**
+     * Age of dog.
+     */
     private int age;
-    /** Breed of dog. */
-    private String breed;
-    /** Name of dog. */
-    private String name;
+    /**
+     * Breed of dog.
+     */
+    private final String breed;
+    /**
+     * Name of dog.
+     */
+    private final String name;
 
     /**
      * Creates a dog object with the specified parameters.
-     * @param name Name of dog
+     *
+     * @param name  Name of dog
      * @param breed Breed of dog
-     * @param age Age of dog
+     * @param age   Age of dog
      */
     public Dog(String name, String breed, int age) {
         this.age = age;
@@ -40,7 +51,8 @@ public class Dog { // TODO
      */
     public static Dog fromFile(String name) {
         // TODO (hint: look at the Utils file)
-        return null;
+        File dogFile = Utils.join(DOG_FOLDER, name);
+        return Utils.readObject(dogFile, Dog.class);
     }
 
     /**
@@ -48,7 +60,7 @@ public class Dog { // TODO
      */
     public void haveBirthday() {
         age += 1;
-        System.out.println(toString());
+        System.out.println(this);
         System.out.println("Happy birthday! Woof! Woof!");
     }
 
@@ -57,13 +69,22 @@ public class Dog { // TODO
      */
     public void saveDog() {
         // TODO (hint: don't forget dog names are unique)
+        File dogFile = Utils.join(DOG_FOLDER, name);
+        if (!dogFile.exists()) {
+            try {
+                dogFile.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        Utils.writeObject(dogFile, this);
     }
 
     @Override
     public String toString() {
         return String.format(
-            "Woof! My name is %s and I am a %s! I am %d years old! Woof!",
-            name, breed, age);
+                "Woof! My name is %s and I am a %s! I am %d years old! Woof!",
+                name, breed, age);
     }
 
 }
